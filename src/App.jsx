3fa0f11a3fec3aaae6a483c3be921f3085b053bc
Misha Knysh <knih19@gmail.com>
2025-05-17 17:01:48 +0200

    add sol

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+
+import { Tabs } from './components/Tabs/Tabs';
 import './App.scss';
 
 export const tabs = [
@@ -9,36 +11,38 @@ export const tabs = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-export const App = () => (
-  <div className="section">
-    <h1 className="title">Selected tab is Tab 1</h1>
+export const App = () => {
+ const [ value, setValue ] = useState(1);
 
-    <div data-cy="TabsComponent">
-      <div className="tabs is-boxed">
-        <ul>
-          <li className="is-active" data-cy="Tab">
-            <a href="#tab-1" data-cy="TabLink">
-              Tab 1
-            </a>
-          </li>
+  return (
+    <div className="section">
+      <h1 className="title">Selected tab is Tab {value}</h1>
 
-          <li data-cy="Tab">
-            <a href="#tab-2" data-cy="TabLink">
-              Tab 2
-            </a>
-          </li>
+      <div data-cy="TabsComponent">
+        <div className="tabs is-boxed">
+          <ul>
+            <li className={value === 1 ? 'is-active' : ''} data-cy="Tab">
+              <a href="#tab-1" data-cy="TabLink" onClick={() => setValue(1)}>
+                Tab 1
+              </a>
+            </li>
 
-          <li data-cy="Tab">
-            <a href="#tab-3" data-cy="TabLink">
-              Tab 3
-            </a>
-          </li>
-        </ul>
-      </div>
+            <li className={value === 2 ? 'is-active' : ''} data-cy="Tab">
+              <a href="#tab-2" data-cy="TabLink" onClick={() => setValue(2)}>
+                Tab 2
+              </a>
+            </li>
 
-      <div className="block" data-cy="TabContent">
-        Some text 1
+            <li className={value === 3 ? 'is-active' : ''} data-cy="Tab">
+              <a href="#tab-3" data-cy="TabLink" onClick={() => setValue(3)}>
+                Tab 3
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <Tabs value={value} />
       </div>
     </div>
-  </div>
-);
+  );
+};
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,13 @@
-export const Tabs = () => {};
+export const Tabs = ({ value }) => {
+  let content = '';
+
+  if (value === 1) {
+    content = 'Some text 1';
+  } else if (value === 2) {
+    content = 'Some text 2';
+  } else {
+    content = 'Some text 3';
+  }
+
+  return <div>{content}</div>;
+};
